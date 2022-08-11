37cdfd865fb4d32ed404d6f16dd49d516758e211
Ethan Goldfarb <cheesical@gmail.com>
2022-08-11 19:50:08 +0000

    lolll

--- bastardizedpetastorm/namedtuple_gt_255_fields.py
+++ bastardizedpetastorm/namedtuple_gt_255_fields.py
@@ -148,6 +148,8 @@ def namedtuple_gt_255_fields(typename, field_names, verbose=False, rename=False,
         if name in seen:
             raise ValueError('Encountered duplicate field name: %r' % name)
         seen.add(name)
+    print("field_names" * 20)
+    print(field_names)
 
     # Fill-in the class template
     class_definition = _class_template.format(
