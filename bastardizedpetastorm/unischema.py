4599e800bb3c6cba1a2d69f8ef3a562133b21488
Ethan Goldfarb <cheesical@gmail.com>
2022-08-11 20:51:06 +0000

    lolll

--- bastardizedpetastorm/unischema.py
+++ bastardizedpetastorm/unischema.py
@@ -365,7 +365,7 @@ class Unischema(object):
                 else:
                     raise
             unischema_fields.append(UnischemaField(column_name, np_type, field_shape, None, arrow_field.nullable))
-        return Unischema('inferred_schema', unischema_fields)
+        return Unischema('inferred_schema?', unischema_fields)
 
     def __getattr__(self, item) -> Any:
         return super().__getattribute__(item)
