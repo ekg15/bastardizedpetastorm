2e724a5cbcc6ca55e6a60738123e64c5dbe104f8
Ethan Goldfarb <cheesical@gmail.com>
2022-08-11 18:59:06 +0000

    lolll

--- bastardizedpetastorm/unischema.py
+++ bastardizedpetastorm/unischema.py
@@ -106,6 +106,7 @@ class _NamedtupleCache(object):
             field_names = list(field_names)
         key = ' '.join([parent_schema_name] + field_names)
         print("here!!!!" * 20)
+        print(_NamedtupleCache._store)
         if key not in _NamedtupleCache._store:
             _NamedtupleCache._store[key] = _new_gt_255_compatible_namedtuple(
                 '{}_view'.format(parent_schema_name), field_names)
